d396e213dd20073c51fbd3d00a8da3982dc8524c
sc16s2w <765012395@qq.com>
2021-11-26 13:37:45 +0100

    "daily work"

---
+++ src/com/company/daily/Code_35.java
@@ -0,0 +1,21 @@
+package com.company.daily;
+
+public class Code_35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int middle = (left+right)/2;
+            if(nums[middle]>target){
+                right = middle-1;
+            }
+            else if(nums[middle]<target){
+                left = middle+1;
+            }
+            else{
+                return middle;
+            }
+        }
+        return left;
+    }
+}
