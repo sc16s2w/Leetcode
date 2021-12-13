db8b13d2f7e23c11c73943ba41b6b51857b7c6ed
sc16s2w <765012395@qq.com>
2021-12-13 16:34:37 +0100

    "daily work"

---
+++ src/com/company/top100/Code_494.java
@@ -0,0 +1,24 @@
+package com.company.top100;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Code_494 {
+    int count = 0;
+
+    public int findTargetSumWays(int[] nums, int S) {
+        calculate(nums, 0, 0, S);
+        return count;
+    }
+
+    public void calculate(int[] nums, int i, int sum, int S) {
+        if (i == nums.length) {
+            if (sum == S) {
+                count++;
+            }
+        } else {
+            calculate(nums, i + 1, sum + nums[i], S);
+            calculate(nums, i + 1, sum - nums[i], S);
+        }
+    }
+}
