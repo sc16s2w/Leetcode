d25ed3fdb13632dbcf1d7ab243e6b8f2e8884ca3
sc16s2w <765012395@qq.com>
2022-01-09 15:54:41 +0100

    "daily work"

--- src/com/company/Array/Code_1295.java
+++ src/com/company/Array/Code_1295.java
@@ -1,4 +1,22 @@
 package com.company.Array;
 
 public class Code_1295 {
+    public int findNumbers(int[] nums) {
+        int result = 0;
+        for(int i =0;i<nums.length;i++){
+            if(countDigit(nums[i])) result++;
+        }
+        return result;
+
+    }
+    public boolean countDigit(int num){
+        boolean result = false;
+        int count = 0;
+        while(num!=0){
+            count++;
+            num = num/10;
+        }
+        if(count%2==0) return true;
+        return result;
+    }
 }
