d69310c0516388f1c186a18a410cb6bde25b7200
sc16s2w <765012395@qq.com>
2022-01-12 10:01:55 +0100

    "daily work"

---
+++ src/com/company/daily/Code_278.java
@@ -0,0 +1,4 @@
+package com.company.daily;
+
+public class Code_278 {
+}
---
+++ src/com/company/daily/Code_704.java
@@ -0,0 +1,21 @@
+package com.company.daily;
+
+public class Code_704 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int middle = (left+right)/2;
+            if(nums[middle] == target){
+                return middle;
+            }
+            else if(nums[middle]<target){
+                left = middle+1;
+            }
+            else{
+                right = middle-1;
+            }
+        }
+        return -1;
+    }
+}
